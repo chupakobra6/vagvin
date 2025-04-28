032b276d940b783c41d77abf5ab459b46e8469ab
chupakobra6 <igorpheik@gmail.com>
2025-04-28 05:55:41 +0300

    Refactor BaseModel import in accounts app
    - Removed the local definition of BaseModel in models.py and imported it from vagvin.models for better code organization and consistency.

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -3,13 +3,7 @@ import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
-class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
-
-    class Meta:
-        abstract = True
+from vagvin.models import BaseModel
 
 
 class User(AbstractUser, BaseModel):
